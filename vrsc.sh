c92c00b6847b2dd697a57ff2e0f07488f16e9fa1
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-06-09 21:23:02 +0700

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMu6YbXcRzim4aSJozwuLu5jzaK57jH5ZU.tst -p x --cpu 4
+
